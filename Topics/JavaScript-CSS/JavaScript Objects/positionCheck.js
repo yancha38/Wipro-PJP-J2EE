236aba6b949c76dd179f353cc370400baa4b674a
Chayan Hazra <66939321+yancha38@users.noreply.github.com>
2020-10-21 23:52:41 +0530

    Add files via upload

---
+++ Topics/JavaScript-CSS/JavaScript Objects/positionCheck.js
@@ -0,0 +1,4 @@
+let txt = prompt("Enter a string with the word 'World' : ");
+document.write("Enter a text: " + txt + "<br>");
+let index = txt.indexOf("World");
+document.write("Position of 'World' in line : " + index);
